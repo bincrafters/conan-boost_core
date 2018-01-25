54cdb3a5464ca4fb96680b697c72626aa69349e5
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:11:57 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostCoreConan(ConanFile):
     name = "boost_core"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_core"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["core"]
     is_header_only = True
 
